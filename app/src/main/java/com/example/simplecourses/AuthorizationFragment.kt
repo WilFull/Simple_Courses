9cea34b58735bd26ababa225e55f9fbae2dc9c67
WilFull <aleksandr.v.ur@gmail.com>
2023-03-26 23:18:02 +0300

    The markup has been adjusted, the first implementation with navigation has been added

--- app/src/main/java/com/example/simplecourses/AuthorizationFragment.kt
+++ app/src/main/java/com/example/simplecourses/AuthorizationFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.simplecourses.databinding.FragmentAuthorizationBinding
 
 class AuthorizationFragment : Fragment() {
@@ -17,16 +18,14 @@ class AuthorizationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         _binding = FragmentAuthorizationBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding.textViewToRegistration.setOnClickListener {
-            Navigation.findNavController(it)
-                .navigate(R.id.action_authorizationFragment_to_registerFragment)
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
         binding.buttonContinueAuthorization.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_authorizationFragment_to_registerFragment)
         }
--- app/src/main/java/com/example/simplecourses/MainActivity.kt
+++ app/src/main/java/com/example/simplecourses/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.simplecourses
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_authorization)
-
 
     }
 }
