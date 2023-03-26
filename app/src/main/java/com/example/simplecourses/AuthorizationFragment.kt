fca3b3626c8abf9dd2b9f7311d7f3386b4d16605
WilFull <aleksandr.v.ur@gmail.com>
2023-03-26 18:26:42 +0300

    added UI for authorization and registration screen

---
+++ app/src/main/java/com/example/simplecourses/AuthorizationFragment.kt
@@ -0,0 +1,40 @@
+package com.example.simplecourses
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.simplecourses.databinding.FragmentAuthorizationBinding
+
+class AuthorizationFragment : Fragment() {
+
+    private var _binding: FragmentAuthorizationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAuthorizationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.textViewToRegistration.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_authorizationFragment_to_registerFragment)
+        }
+        binding.buttonContinueAuthorization.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_authorizationFragment_to_registerFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
--- app/src/main/java/com/example/simplecourses/MainActivity.kt
+++ app/src/main/java/com/example/simplecourses/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.simplecourses
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_authorization)
+
+
     }
 }
