8b8271df5a3bada71c3d3be5ece1c0f659de63bb
Alex <aleksandr.v.ur@yandex.ru>
2023-05-07 22:45:33 +0300

    Preparing to connect to develop. Подготовка к соединению с develop.

--- app/src/main/java/com/example/simplecourses/firebase/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/simplecourses/firebase/auth/RegisterFragment.kt
@@ -1,4 +1,4 @@
-package com.example.simplecourses.registration.view
+package com.example.simplecourses.firebase.auth
 
 import android.content.Context
 import androidx.lifecycle.ViewModelProvider
@@ -8,8 +8,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.view.get
 import androidx.core.view.isEmpty
 import androidx.navigation.fragment.findNavController
 import com.example.simplecourses.R
